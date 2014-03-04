b35c3ccded70f2da73a24166df24bac5cf157256
Sarah Vessels <sarah.vessels@cirrusmio.com>
2014-03-04 18:34:13 -0500

    Add Arcadium location seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+puts 'Seeding locations'
+puts '- Arcadium'
+Location.create!(name: 'Arcadium',
+                 street_address: '574 N Limestone St',
+                 city: 'Lexington',
+                 postal_code: '40508',
+                 state: 'Kentucky',
+                 phone: '(859) 317-9712',
+                 website: 'http://www.arcadiumbar.com/',
+                 kid_friendly: false,
+                 latitude: 38.053789,
+                 longitude: -84.486492,
+                 hours: 'Mon - Thurs: 5:00pm - 1:00am, Friday: 4:00pm - 2:30am, Saturday: 2:00pm - 2:30am, Sunday: 1:00pm - 12:00am')
