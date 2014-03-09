0e3e7b26062739d96ff986ded93c9a3c72bcd512
Eli Riveire Warner <riveire@gmail.com>
2014-03-09 14:10:39 -0400

    Added resources :locations

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Lexplaypacman::Application.routes.draw do
   resources :players
+  resources :locations
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
