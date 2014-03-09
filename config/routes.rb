559a11f6aced8bdf52da1513f2369345191dcc14
Sarah Vessels <sarah.vessels@cirrusmio.com>
2014-03-09 14:11:56 -0400

    Herp derp there was already a resources :players

--- config/routes.rb
+++ config/routes.rb
@@ -18,8 +18,6 @@ Lexplaypacman::Application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
-  resources :players
-
   # Example resource route with options:
   #   resources :products do
   #     member do
