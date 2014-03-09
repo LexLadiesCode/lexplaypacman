e48c9836ada05ba5d3caf56e1a9570ccbb37dd86
beckyjo <rebecca.jude@gmail.com>
2014-03-09 15:32:08 -0400

    Added drop downs for player and location

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,7 @@
 class Location < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
-  has_many :standing
+  has_many :standings
+  def description
+  	"#{name} - #{street_address}, #{city}"
+  end
 end
