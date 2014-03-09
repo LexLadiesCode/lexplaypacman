daf47e7c5180a59fff49a8afbb87506c9a269fcc
beckyjo <rebecca.jude@gmail.com>
2014-03-09 14:20:33 -0400

    Deleted comment

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,6 @@ Lexplaypacman::Application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
-  resources :players
 
   # Example resource route with options:
   #   resources :products do
