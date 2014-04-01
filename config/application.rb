84dd8ccb33c639916b23216e4d201fbc05af185e
Eli Riveire Warner <riveire@gmail.com>
2014-03-31 22:18:47 -0400

    time zone

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,5 @@ module Lexplaypacman
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.time_zone = 'Eastern Time (US & Canada)'
-    config.active_record.default_timezone = 'Eastern Time (US & Canada)'
   end
 end
