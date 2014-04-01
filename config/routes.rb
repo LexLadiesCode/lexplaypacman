7ba24b14b1f78a7612c05fed28be0c2be0d2f130
Sarah Vessels <sarah.vessels@cirrusmio.com>
2014-04-01 18:44:23 -0400

    Only location index route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Lexplaypacman::Application.routes.draw do
-  get "about" => "pages#about"
+  get '/about' => 'pages#about', as: :about
 
   resources :standings, only: [:show, :new, :index, :create] do
     collection do
@@ -9,7 +9,7 @@ Lexplaypacman::Application.routes.draw do
 
   resources :players, only: [:show, :new, :index, :create]
 
-  resources :locations
+  resources :locations, only: [:index]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
